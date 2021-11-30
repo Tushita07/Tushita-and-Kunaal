4c84043491b54a32a2a3fe201f0efd39a2cff262
kunaalgoel1 <88011458+kunaalgoel1@users.noreply.github.com>
2021-11-30 11:11:57 -0600

    blah

--- main.py
+++ main.py
@@ -3,6 +3,20 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from neural import NeuralNet
+
+# each row is an (input, output) tuple
+xor_data = [
+    #   input     output    corresponding example
+    ([0.0, 0.0],  [0.0]),  #[0, 0] => 0
+    ([0.0, 1.0],  [1.0]),  #[0, 1] => 1
+    ([1.0, 0.0],  [1.0]),  #[1, 1] => 1
+    ([1.0, 1.0],  [0.0])   #[1, 0] => 0
+]
+
+
+nn = NeuralNet(2, 5, 1)
+nn.train(xor_data)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +25,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #print_hi('PyCharm')
+    nn = NeuralNet(2, 5, 1)
+    nn.train(xor_data)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
